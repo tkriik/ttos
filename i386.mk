59f9ac287f29758092ee2962ab858c2d4b66dd21
Tanel Kriik <tanel.kriik@gmail.com>
2018-01-08 15:55:20 +0200

    Reorganize build settings

--- i386.mk
+++ i386.mk
@@ -1,4 +1,5 @@
-include kernel.mk
+ARCH_CFLAGS=	-m32
+ARCH_LDFLAGS=	-m elf_i386
 
 BOOT_SRC=	boot_i386.nasm
 BOOT_BIN=	boot_i386.bin
@@ -9,17 +10,13 @@ IMG=		ttos_i386.img
 
 img: $(IMG)
 
+include kernel.mk
+
 $(IMG): $(KERNEL_BIN) $(BOOT_BIN)
 	cat $(BOOT_BIN) $(KERNEL_BIN) > $(IMG)
 
 $(BOOT_BIN): $(BOOT_SRC)
 	nasm -DKERNEL_OFFSET=$(KERNEL_OFFSET) -o $(BOOT_BIN) $(BOOT_SRC)
 
-$(KERNEL_OBJ): $(KERNEL_SRC)
-	gcc $(KERNEL_CFLAGS) -m32 -c $(KERNEL_SRC)
-
-$(KERNEL_BIN): $(KERNEL_OBJ)
-	ld -m elf_i386 -o $(KERNEL_BIN) -Ttext $(KERNEL_OFFSET) $(KERNEL_OBJ) --oformat binary
-
-clean:
-	rm -f $(IMG) $(BOOT_BIN) $(KERNEL_BIN) $(KERNEL_OBJ)
+clean: kernel_clean
+	rm -f $(IMG) $(BOOT_BIN)
--- kernel.mk
+++ kernel.mk
@@ -1,10 +1,8 @@
 KERNEL_SRC=	kernel.c
-
 KERNEL_OBJ=	kernel.o
-
 KERNEL_BIN=	kernel.bin
-
-KERNEL_CFLAGS=	-std=c99 \
+KERNEL_CFLAGS=	-c \
+		-std=c99 \
 		-pedantic \
 		-pedantic-errors \
 		-Wall \
@@ -14,3 +12,12 @@ KERNEL_CFLAGS=	-std=c99 \
 		-fno-stack-protector \
 		-nostdlib \
 		-ffreestanding
+
+kernel.o: kernel.c
+	$(CC) $(KERNEL_CFLAGS) $(ARCH_CFLAGS) $< -o $@
+
+$(KERNEL_BIN): $(KERNEL_OBJ)
+	ld -o $(KERNEL_BIN) -Ttext $(KERNEL_OFFSET) $(ARCH_LDFLAGS) $(KERNEL_OBJ) --oformat binary
+
+kernel_clean:
+	rm -f $(KERNEL_OBJ) $(KERNEL_BIN)
